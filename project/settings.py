3e31a30036f708378a74b23ca87e72e0c65ad1b7
Qeybulla Aliverdiyev <qeybulla39@mail.ru>
2022-06-24 14:13:14 +0400

    changed prod

--- project/settings.py
+++ project/settings.py
@@ -17,7 +17,7 @@ from celery.schedules import crontab
 import project.tasks
 from datetime import timedelta
 
-PROD = False
+PROD = True
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
